29a3e66970572c6852017150c5261d2d57ac1ff8
Stephan Joseph <stephanjoseph2007@gmail.com>
2016-11-21 15:27:34 -0500

    updated

--- public/main.js
+++ public/main.js
@@ -1,6 +1,19 @@
-const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
+let hue = 0
+let sat = 100
+let light = 50
+
+const getHSL = () => {
+  return 'hsl(${hue}), ${sat}%, ${light}%)'
 }
 
+const main = () => {
+  document.addEventListener
+  const hueInput = document.getElementById('hue')
+  hueInput.addEventListener('input', () => {
+    hue = hueInput.value
+    document.querySelector('.sec1').style.backgrounColor = getHSL()
+  })
+  document.getElementById('sat')
+  document.getElementById('light')
+}
 document.addEventListener('DOMContentLoaded', main)
-
