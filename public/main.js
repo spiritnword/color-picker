36962f27c840078db565270d0c9fa1887893ab7d
Stephan Joseph <stephanjoseph2007@gmail.com>
2016-11-21 18:24:04 -0500

    update

--- public/main.js
+++ public/main.js
@@ -1,19 +1,28 @@
-let hue = 0
-let sat = 100
-let light = 50
 
-const getHSL = () => {
-  return 'hsl(${hue}), ${sat}%, ${light}%)'
+const somethingChanged = () => {
+  // Find the value of the hue
+  let hueValue = document.querySelector('input[name=hue]').value
+  // Find the value of the saturation
+  let saturationValue = document.querySelector('input[name=saturation]').value
+  // Find the value of the lightness
+  let lightnessValue = document.querySelector('input[name=lightness]').value
+  // Find the value of the alpha
+
+  // Build a background color string in HSL form
+  let hslBackgroundColor = `hsl(${hueValue}, ${saturationValue}%, ${lightnessValue}%)`
+
+  // Go find our box we called `sec1` in the DOM
+  let sec1 = document.getElementById('sec1')
+
+  // Put that style as the background color
+  sec1.style.backgroundColor = hslBackgroundColor
 }
 
 const main = () => {
-  document.addEventListener
-  const hueInput = document.getElementById('hue')
-  hueInput.addEventListener('input', () => {
-    hue = hueInput.value
-    document.querySelector('.sec1').style.backgrounColor = getHSL()
-  })
-  document.getElementById('sat')
-  document.getElementById('light')
+  let sliders = document.querySelectorAll('input[name]')
+  for (var i = 0; i < sliders.length; i++) {
+    sliders[i].addEventListener('input', somethingChanged)
+  }
 }
+
 document.addEventListener('DOMContentLoaded', main)
